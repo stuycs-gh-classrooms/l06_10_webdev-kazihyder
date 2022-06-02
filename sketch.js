4ead6c24ea8b5631db0bd36cd49f88703d49deca
kazihyder <90656647+kazihyder@users.noreply.github.com>
2022-06-02 15:07:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  // put setup code here
+createCanvas(800, 800);
+  background(255);
 }
 
 function draw() {
-  // put drawing code here
+  stroke(204, 102, 0);
+
+  circle(220, 180, 220);
+
+  line(10, 10, 50, 100);
+
+  rect( 480, 400, 300, 300);
 }
