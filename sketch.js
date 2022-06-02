5d9eb05583ed8bfc711c06efba41de8527e906e6
kazihyder <90656647+kazihyder@users.noreply.github.com>
2022-06-02 15:25:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
 function setup() {
-createCanvas(100, 100;
+createCanvas(100, 100);
  
 }
 
 function draw() {
- background(0,128,0);
-  
+ background(0,0,0);
  rectangle(50,50,75,75):
  color('green');
   circle(25,25,10); 
