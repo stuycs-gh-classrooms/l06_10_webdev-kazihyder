dab6d9144489ce8bda24fe46d7069ef7a8bf8678
kazihyder <90656647+kazihyder@users.noreply.github.com>
2022-06-02 15:34:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ createCanvas(500, 500);
 }
 
 function draw() {
- background(220);
- circle(250,250,100); 
-fill('red');
+  background(220);
+    let c = color('green');
+    fill(c);
+    circle(200, 100, 150); 
 }
